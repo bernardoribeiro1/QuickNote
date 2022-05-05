dfa6e48ffabdfc27cabf40242c7505b384e8280f
Bernardo Ribeiro <bernardoribeiro@cpejr.com.br>
2022-05-05 15:56:12 -0300

    Mais_variaveis

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
+//Declarar variavel
+//let
+//const é a melhor
+//var
 
+let numero = 0;
 
+function clicou(dividir){
+    numero += 1;
+    let dividido; //melhor jeito é delcarar antes e usar o LET 
+
+    if(dividir === true){
+        // var dividido = numero / 2;//var tem escopo global, o let não, mas não é bom usar
+        dividido = numero / 2;
+    }
+
+    alert(numero);
+    alert(dividido);
+}
 function App(){
-    const var_frase = "String";
-    const var_letra = "C";
-    const var_num = 63;
 
-    return (
+    return(
         <div>
-            <h1>Segunda Aula</h1>
-            <h2>{var_frase}</h2>
-            <h2>{var_letra + "LS"} {var_num + 2}</h2>
-            <button>Clique aqui</button>
+        <h1>Mais coisa de variaveis</h1>
+        <button onClick ={() => clicou(false)}
+        >+ um
+        </button>
 
+        <button onClick ={() => clicou(true)}
+        >+ um e divide
+        </button>
+            
         </div>
     );
 }
